980722c39fc752de9e09ff0667d3513f67704a42
kelvsc <150187892+kelvsc@users.noreply.github.com>
2023-11-07 16:08:15 -0500

    Add files via upload

---
+++ 01-prep-01.sql
@@ -0,0 +1,23 @@
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2022_10_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2022_11_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2022_12_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_01_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_02_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_03_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_04_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_05_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_06_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_07_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_08_data
+UNION ALL
+SELECT * FROM cs-bike-share-analysis.Trip_Data.2023_09_data
